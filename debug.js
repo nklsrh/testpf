0d0eb77f04f8be31f9c08adfe16a9951da239bb1
Nikhil Suresh <nklsrh@gmail..com>
2021-01-31 16:27:48 +0000

    debug set currency

--- debug.js
+++ debug.js
@@ -29,6 +29,41 @@ handlers.DebugAddCurrency = function(args)
 };
 
 
+
+handlers.DebugSetCurrency = function(args) 
+{
+	var GetUserInventoryRequest = 
+	{
+        "PlayFabId": currentPlayerId
+    };
+	
+    var cur = args.currency;
+	var amt = args.amount;
+
+	log.info("cur amt " + cur + " : " + amt);
+	
+    var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
+	var userVcBalances = GetUserInventoryResult.VirtualCurrency;
+
+	var delta = amt - userVcBalances[cur];
+	
+	if (delta >= 0)
+	{
+		AddVc(userVcBalances, cur, delta);
+	}
+	else
+	{
+		SubtractVc(userVcBalances, cur, delta);
+	}
+
+	var results = {};
+		results.result = true;
+
+	return JSON.stringify(results);
+};
+
+
+
 //TEMPORARY solution until PlayFab's log functions are available from CloudScript
 function Log(message, data) {
     server.SetTitleData({
